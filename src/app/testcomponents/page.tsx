653f8eb2d585fb27fc616916ca664a2604a882fc
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2025-02-08 13:35:31 -0300

    Test components page

--- src/app/testcomponents/page.tsx
+++ src/app/testcomponents/page.tsx
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
-
 export default function TestComponents(){
-    function handleDownload(){
-        window.open(`/api/generate-pdf?imobillevalue=500000&financementvalue=420000&parcels=420`, '_blank')
+    function testAmortizationSAC(){        
+        window.open(`/api/generate-pdf?imobillevalue=${50000000}&financementvalue=${40000000}&parcels=${320}&amortization=${'SAC'}`, '_blank')
     }
     return(
-        <button onClick={handleDownload}>Baixar PDF</button>
+        <button onClick={testAmortizationSAC}>Baixar PDF</button>
     )
 }
