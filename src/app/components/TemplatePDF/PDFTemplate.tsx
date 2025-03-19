69bce09abece15d7d674347bd6c50c08b774a47e
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2025-03-19 13:16:17 -0300

    Fix useless imports

--- src/app/components/TemplatePDF/PDFTemplate.tsx
+++ src/app/components/TemplatePDF/PDFTemplate.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import { PdfProps } from '@/utils/pdfProps'
 import { TableProps } from '@/utils/tableProps';
-import { Page, Text, View, Document, StyleSheet,Font} from '@react-pdf/renderer'
+import { Page, Text, View, Document, StyleSheet} from '@react-pdf/renderer'
 
 
 const styles = StyleSheet.create({
