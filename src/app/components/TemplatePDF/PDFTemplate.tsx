7cde515eaef46f4d9af3147631c42185604ee0fc
Luan-Lopes-da-Silva <sangbombr35@gmail.com>
2025-03-20 13:21:13 -0300

    Align tables and conditionals

--- src/app/components/TemplatePDF/PDFTemplate.tsx
+++ src/app/components/TemplatePDF/PDFTemplate.tsx
@@ -7,14 +7,14 @@ const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
     backgroundColor: '#E4E4E4',
-    padding: '20px',
     width: '100%',
   },
 
   container: {
     width: '100%',
     backgroundColor: '#868686',
-    padding: '60px 20px',
+    height:'100%',
+    padding: '12px',
   },
 
   rowSummary: {
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
   },
 
   headerDiv: {
-    width: 400,
+    width: 397,
     height: 100,
     backgroundColor: '#028DA6',
     justifyContent: 'center',
@@ -44,25 +44,76 @@ const styles = StyleSheet.create({
 
   rowHeaders: {
     flexDirection: 'row',
-    width: '100%',
+    minWidth:300,
     height: 70,
-    justifyContent: 'space-between',
     padding: 20,
     backgroundColor: '#D3D664',
+    alignItems:"center",
+  },
+
+  rowHeadersAlt: {
+    flexDirection: 'row',
+    height: 70,
+    width:350,
+    padding: 20,
+    backgroundColor: '#D3D664',
+    alignItems:"center",
   },
 
   headerSubTitles: {
     fontSize: 16,
     color: '#000',
+    width: '33%',
+    textAlign: 'center',
+  },
+  headerSubTitlesData:{
+    fontSize: 16,
+    color: '#000',
+    width: '33%',
+    textAlign: 'center',
+  },
+  headerSubTitlesData2:{
+    fontSize: 16,
+    color: '#000',
+    width: '100%',
+    textAlign: 'center',
+  },
+  headerSubTitlesDataAlt:{
+    fontSize: 16,
+    color: '#000',
+    width: '50%',
+    textAlign: 'center',
+  },
+  headerSubTitlesAlt: {
+    fontSize: 16,
+    color: '#000',
+    width: '50%',
+    textAlign: 'center',
+  },
+  headerSubTitlesAlt2: {
+    fontSize: 16,
+    color: '#000',
+    width: '100%',
+    textAlign: 'center',
   },
 
   rowDatas: {
-    width: '100%',
+    minWidth:300,
     height: 70,
     flexDirection: 'row',
     justifyContent: 'space-between',
     backgroundColor: '#028DA6',
     padding: 20,
+    alignItems:'center'
+  },
+  rowDatasAlt: {
+    width:350,
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#028DA6',
+    padding: 20,
+    alignItems:'center'
   },
 
   titleTable: {
@@ -77,8 +128,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     width: '100%',
     justifyContent: 'space-between',
-    marginBottom: 12,
     backgroundColor: '#D3D664',
+    alignItems:'center',
+    padding:12
   },
 
   rowDiv2: {
@@ -86,18 +138,18 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'space-between',
     backgroundColor: '#028DA6',
-    padding: 10,
+    padding: 12,
   },
 
   rowTd: {
-    fontSize: 18,
+    fontSize: 14,
     color: '#FFF',
     width: '12%', // Define largura para um melhor alinhamento
     textAlign: 'center',
   },
 
   rowTh: {
-    fontSize: 16,
+    fontSize: 14,
     color: '#FFF',
     width: '12%', // Define largura para um melhor alinhamento
     textAlign: 'center',
@@ -114,6 +166,61 @@ export function PDFTemplate({ financementValue, imobilleValue, parcels, amortiza
   let dueBalance = financementValue;
   const amortizationInSac = financementValue / parcels;
 
+
+
+  const currentDate = new Date()
+
+  const currentDay = currentDate.getDate()<10?`0${currentDate.getDate()}`:currentDate.getDate()
+  const currentMonth = currentDate.getMonth()<10?`0${currentDate.getMonth()+1}`:currentDate.getMonth()+1
+  const currentYear = currentDate.getFullYear()
+  const newDate = `${currentDay}/${currentMonth}/${currentYear}`
+
+
+  function aplicarMascara(value:number,cutTarget:number) {
+    let valorStr = value.toString().slice(0, cutTarget);
+    
+    let [parteInteira, parteDecimal] = valorStr.split('.');
+    
+    parteInteira = parteInteira.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    
+    if (parteDecimal) {
+        return `${parteInteira},${parteDecimal}`;
+    } else {
+        return parteInteira;
+    }
+}
+
+  function calcularTabelaPrice(valorFinanciado:number, taxaJurosAnual:number, prazo:number) {
+    const taxaJurosMensal = (taxaJurosAnual / 100) / 12;
+    const numeroParcelas = prazo;
+    
+    const parcela = valorFinanciado * (taxaJurosMensal * Math.pow(1 + taxaJurosMensal, numeroParcelas)) / 
+                    (Math.pow(1 + taxaJurosMensal, numeroParcelas) - 1);
+
+    let saldoDevedor = valorFinanciado;
+    const tabela:TableProps[] = [];
+
+    for (let i = 1; i <= numeroParcelas; i++) {
+        const juros = saldoDevedor * taxaJurosMensal;
+        const amortizacao = parcela - juros;
+        saldoDevedor -= amortizacao;
+
+        tabela.push({
+            parcel: i,
+            parcelValue:parcela,
+            taxs: juros,
+            amortization: amortizacao,
+            dueBalance: saldoDevedor,
+            Dfi: `0`,
+            Mip:`0`,
+            Tsa:`0`
+        });
+    }
+
+    return tabela;
+}
+
+const priceTable = calcularTabelaPrice(financementValue,11.90,parcels)
   for (let index = 0; index < parcels; index++) {
     const taxsMonth = (dueBalance * 0.119).toString().slice(0, 3);
     const createNewItem: TableProps = {
@@ -122,14 +229,13 @@ export function PDFTemplate({ financementValue, imobilleValue, parcels, amortiza
       Mip: `0`,
       Tsa: `0`,
       dueBalance: dueBalance -= amortizationInSac,
-      parcel: index + 1, // Ajuste para começar a partir da parcela 1
+      parcel: index + 1,
       taxs: Number(taxsMonth),
       parcelValue: amortizationInSac + Number(taxsMonth),
     };
     arrayEachRowDiv.push(createNewItem);
   }
 
-  // Função para dividir a tabela em grupos de 20 itens por página
   const chunkArray = (arr: TableProps[], chunkSize: number) => {
     const result: TableProps[][] = [];
     for (let i = 0; i < arr.length; i += chunkSize) {
@@ -138,7 +244,8 @@ export function PDFTemplate({ financementValue, imobilleValue, parcels, amortiza
     return result;
   };
 
-  const rowsPerPage = 20;
+  const rowsPerPage = 40;
+  const paginatedPriceTable = chunkArray(priceTable,rowsPerPage)
   const paginatedRows = chunkArray(arrayEachRowDiv, rowsPerPage);
 
   return (
@@ -156,9 +263,9 @@ export function PDFTemplate({ financementValue, imobilleValue, parcels, amortiza
                 <Text style={styles.headerSubTitles}>Valor financiado</Text>
               </View>
               <View style={styles.rowDatas}>
-                <Text style={styles.headerSubTitles}>{imobilleValue}</Text>
-                <Text style={styles.headerSubTitles}>{imobilleValue - financementValue}</Text>
-                <Text style={styles.headerSubTitles}>{financementValue}</Text>
+                <Text style={styles.headerSubTitlesData}>R$ {aplicarMascara(imobilleValue,9)},00</Text>
+                <Text style={styles.headerSubTitlesData}>R$ {aplicarMascara((imobilleValue - financementValue),9)},00</Text>
+                <Text style={styles.headerSubTitlesData}>R$ {aplicarMascara(financementValue,9)},00</Text>
               </View>
             </View>
 
@@ -172,9 +279,9 @@ export function PDFTemplate({ financementValue, imobilleValue, parcels, amortiza
                 <Text style={styles.headerSubTitles}>CET - ANUAL</Text>
               </View>
               <View style={styles.rowDatas}>
-                <Text style={styles.headerSubTitles}>Olá</Text>
-                <Text style={styles.headerSubTitles}>Olá</Text>
-                <Text style={styles.headerSubTitles}>Olá</Text>
+                <Text style={styles.headerSubTitlesData}>11.90 %</Text>
+                <Text style={styles.headerSubTitlesData}>0.119</Text>
+                <Text style={styles.headerSubTitlesData}>Olá</Text>
               </View>
             </View>
           </View>
@@ -187,9 +294,9 @@ export function PDFTemplate({ financementValue, imobilleValue, parcels, amortiza
                 <Text style={styles.headerSubTitles}>IOF</Text>
               </View>
               <View style={styles.rowDatas}>
-                <Text style={styles.headerSubTitles}>{expanses}</Text>
-                <Text style={styles.headerSubTitles}>Olá</Text>
-                <Text style={styles.headerSubTitles}>Olá</Text>
+                <Text style={styles.headerSubTitlesData}>R$ {aplicarMascara(expanses,9)},00</Text>
+                <Text style={styles.headerSubTitlesData}>Olá</Text>
+                <Text style={styles.headerSubTitlesData}>Olá</Text>
               </View>
             </View>
 
@@ -197,70 +304,107 @@ export function PDFTemplate({ financementValue, imobilleValue, parcels, amortiza
               <View style={styles.rowHeaders}>
                 <Text style={styles.headerSubTitles}>Renda</Text>
                 <Text style={styles.headerSubTitles}>Prazo</Text>
-                <Text style={styles.headerSubTitles}>Sistema Amort</Text>
+                <Text style={styles.headerSubTitles}>Sistema Amortização</Text>
               </View>
               <View style={styles.rowDatas}>
-                <Text style={styles.headerSubTitles}>Olá</Text>
-                <Text style={styles.headerSubTitles}>{parcels}</Text>
-                <Text style={styles.headerSubTitles}>{amortization}</Text>
+                <Text style={styles.headerSubTitlesData}>Olá</Text>
+                <Text style={styles.headerSubTitlesData}>{parcels}</Text>
+                <Text style={styles.headerSubTitlesData}>{amortization}</Text>
               </View>
             </View>
           </View>
 
           <View style={styles.rowSummary}>
             <View style={styles.summary}>
-              <View style={styles.rowHeaders}>
-                <Text style={styles.headerSubTitles}>Tipo imóvel</Text>
+              <View style={styles.rowHeadersAlt}>
+                <Text style={styles.headerSubTitlesAlt2}>Tipo imóvel</Text>
               </View>
-              <View style={styles.rowDatas}>
-                <Text style={styles.headerSubTitles}>Olá</Text>
+              <View style={styles.rowDatasAlt}>
+                <Text style={styles.headerSubTitlesData2}>Residencial</Text>
               </View>
             </View>
 
             <View style={styles.summary}>
-              <View style={styles.rowHeaders}>
-                <Text style={styles.headerSubTitles}>Seguradora</Text>
-                <Text style={styles.headerSubTitles}>Data da simulação</Text>
+              <View style={styles.rowHeadersAlt}>
+                <Text style={styles.headerSubTitlesAlt}>Seguradora</Text>
+                <Text style={styles.headerSubTitlesAlt}>Data da simulação</Text>
               </View>
-              <View style={styles.rowDatas}>
-                <Text style={styles.headerSubTitles}>Olá</Text>
-                <Text style={styles.headerSubTitles}>Olá</Text>
+              <View style={styles.rowDatasAlt}>
+                <Text style={styles.headerSubTitlesDataAlt}>Olá</Text>
+                <Text style={styles.headerSubTitlesDataAlt}>{newDate}</Text>
               </View>
             </View>
           </View>
         </View>
       </Page>
 
-      {/* Renderizando as páginas com as parcelas */}
-      {paginatedRows.map((pageRows, index) => (
-        <Page size="A3" style={styles.page} key={index}>
-          <View style={styles.container}>
-            <Text style={styles.titleTable}>Parcelas</Text>
-            <View style={styles.rowHeaderTable}>
-              <Text style={styles.rowTh}>Parcela</Text>
-              <Text style={styles.rowTh}>Amortização</Text>
-              <Text style={styles.rowTh}>Juros</Text>
-              <Text style={styles.rowTh}>Seguro MIP</Text>
-              <Text style={styles.rowTh}>Seguro DFI</Text>
-              <Text style={styles.rowTh}>TSA</Text>
-              <Text style={styles.rowTh}>Valor parcela</Text>
-              <Text style={styles.rowTh}>Saldo devedor</Text>
-            </View>
-            {pageRows.map((item, itemIndex) => (
-              <View key={itemIndex} style={styles.rowDiv2}>
-                <Text style={styles.rowTd}>{item.parcel}</Text>
-                <Text style={styles.rowTd}>{`${item.amortization.toString().slice(0,7)}`}</Text>
-                <Text style={styles.rowTd}>{`${item.taxs.toFixed(2)}`}</Text>
-                <Text style={styles.rowTd}>{item.Mip}</Text>
-                <Text style={styles.rowTd}>{item.Dfi}</Text>
-                <Text style={styles.rowTd}>{item.Tsa}</Text>
-                <Text style={styles.rowTd}>{`${item.parcelValue.toString().slice(0,7)}`}</Text>
-                <Text style={styles.rowTd}>{`${item.dueBalance.toString().slice(0,6)}`}</Text>
+      {amortization=='PRICE'?(
+        <>
+          {paginatedPriceTable.map((priceTable,index)=>(
+              <Page size="A3" style={styles.page} key={index}>
+              <View style={styles.container}>
+              <Text style={styles.titleTable}>Parcelas</Text>
+                <View style={styles.rowHeaderTable}>
+                  <Text style={styles.rowTh}>Parcela</Text>
+                  <Text style={styles.rowTh}>Amortização</Text>
+                  <Text style={styles.rowTh}>Juros</Text>
+                  <Text style={styles.rowTh}>Seguro MIP</Text>
+                  <Text style={styles.rowTh}>Seguro DFI</Text>
+                  <Text style={styles.rowTh}>TSA</Text>
+                  <Text style={styles.rowTh}>Valor parcela</Text>
+                  <Text style={styles.rowTh}>Saldo devedor</Text>
+                </View>
+  
+                {priceTable.map((item,index)=>(
+                  <View style={styles.rowDiv2} key={index}>
+                      <Text style={styles.rowTd}>{item.parcel}</Text>
+                      <Text style={styles.rowTd}>R$ {aplicarMascara(item.amortization,7)}</Text>
+                      <Text style={styles.rowTd}>R$ {aplicarMascara(item.taxs,7)}</Text>
+                      <Text style={styles.rowTd}>R$ 0,00</Text>
+                      <Text style={styles.rowTd}>R$ 0,00</Text>
+                      <Text style={styles.rowTd}>R$ 0,00</Text>
+                      <Text style={styles.rowTd}>R$ {aplicarMascara(item.parcelValue,7)}</Text>
+                      <Text style={styles.rowTd}>R$ {aplicarMascara(item.dueBalance,9)}</Text>
+                  </View>
+                ))}
               </View>
-            ))}
-          </View>
-        </Page>
-      ))}
+          </Page>
+          ))}
+        </>
+        
+      ):(
+        <>
+          {paginatedRows.map((pageRows, index) => (
+          <Page size="A3" style={styles.page} key={index}>
+            <View style={styles.container}>
+              <Text style={styles.titleTable}>Parcelas</Text>
+              <View style={styles.rowHeaderTable}>
+                <Text style={styles.rowTh}>Parcela</Text>
+                <Text style={styles.rowTh}>Amortização</Text>
+                <Text style={styles.rowTh}>Juros</Text>
+                <Text style={styles.rowTh}>Seguro MIP</Text>
+                <Text style={styles.rowTh}>Seguro DFI</Text>
+                <Text style={styles.rowTh}>TSA</Text>
+                <Text style={styles.rowTh}>Valor parcela</Text>
+                <Text style={styles.rowTh}>Saldo devedor</Text>
+              </View>
+              {pageRows.map((item, itemIndex) => (
+                <View key={itemIndex} style={styles.rowDiv2}>
+                  <Text style={styles.rowTd}>{item.parcel}</Text>
+                  <Text style={styles.rowTd}>R$ {`${aplicarMascara(item.amortization,7)}`}</Text>
+                  <Text style={styles.rowTd}>R$ {`${aplicarMascara(item.taxs,7)}`}</Text>
+                  <Text style={styles.rowTd}>R$ {item.Mip}</Text>
+                  <Text style={styles.rowTd}>R$ {item.Dfi}</Text>
+                  <Text style={styles.rowTd}>R$ {item.Tsa}</Text>
+                  <Text style={styles.rowTd}>R$ {`${aplicarMascara(item.parcelValue,7)}`}</Text>
+                  <Text style={styles.rowTd}>R$ {`${aplicarMascara(item.dueBalance,9)}`}</Text>
+                </View>
+              ))}
+            </View>
+          </Page>
+        ))}
+        </>
+      )}
     </Document>
   );
 }
